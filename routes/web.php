629f7a2935948e748890c06d46a70f1612eb7a07
renier.trenuela@freighttrackertms.com <renier.trenuela@freighttrackertms.com>
2025-09-24 13:53:11 +0800

    complete the features

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Livewire\ImageOptimizer;
+use App\Livewire\JsCssMinifier;
+use App\Livewire\PdfOptimizer;
+use App\Livewire\ZipCompressor;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);;
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/image', ImageOptimizer::class)->name('image');
+Route::get('/zip', ZipCompressor::class)->name('zip');
+Route::get('/pdf', PdfOptimizer::class)->name('pdf');
+Route::get('/minify', JsCssMinifier::class)->name('minify');
